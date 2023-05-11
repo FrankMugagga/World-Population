7871fe84c5f21c145eae8f4afb92d14c0af400ae
FrankMugagga <frank.mugagga@gmail.com>
2023-05-12 00:49:36 +0300

    Had a problem with jest and unit testing so i wanted to set script modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 import './index.css';
@@ -8,9 +9,11 @@ import './index.css';
 ReactDOM.render(
 
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
