d11d2fde3f83262933620497c3d24b347076e309
FrankMugagga <frank.mugagga@gmail.com>
2023-05-12 00:48:09 +0300

    Set up the project render and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Continents from './features/countries/continents';
+import Countries from './features/countries/country/Country';
+import DetailsCountry from './features/countries/countryDetails/DetailsCountry';
+import NotFound from './features/countries/Notfound';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Continents />} />
+        <Route path="/countries/:continent" element={<Countries />} />
+        <Route path="/countryDetail/:country" element={<DetailsCountry />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
